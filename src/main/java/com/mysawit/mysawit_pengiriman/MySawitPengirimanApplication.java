744488cf1a3434797831d60a2a7f8f5fcb9218c3
NaviCo-de <aridarrell30@gmail.com>
2026-02-17 23:23:20 +0700

    Early Setup

---
+++ src/main/java/com/mysawit/mysawit_pengiriman/MySawitPengirimanApplication.java
@@ -0,0 +1,13 @@
+package com.mysawit.mysawit_pengiriman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySawitPengirimanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySawitPengirimanApplication.class, args);
+	}
+
+}
