8876506ef9985af6d3fe2cff398d4234e269929a
NaviCo-de <aridarrell30@gmail.com>
2026-03-06 09:36:32 +0700

    Implement TDD Green

--- src/main/java/com/mysawit/mysawit_pengiriman/service/PengirimanService.java
+++ src/main/java/com/mysawit/mysawit_pengiriman/service/PengirimanService.java
@@ -1,13 +1,16 @@
 package com.mysawit.mysawit_pengiriman.service;
 
 import com.mysawit.mysawit_pengiriman.model.Pengiriman;
+import com.mysawit.mysawit_pengiriman.model.StatusPengiriman;
+
 import java.util.List;
 import java.util.UUID;
 
 public interface PengirimanService {
-    Pengiriman create(Pengiriman pengiriman);
-    List<Pengiriman> findAll();
-    Pengiriman findById(UUID id);
-    Pengiriman update(UUID id, Pengiriman pengirimanDetails);
-    void delete(UUID id);
+    public Pengiriman assignPengiriman(Pengiriman pengiriman);
+    public Pengiriman updateStatusBySupir(UUID id, StatusPengiriman statusBaru);
+    public Pengiriman verifikasiOlehMandor(UUID id, boolean isApproved, String alasan);
+    public Pengiriman verifikasiOlehAdmin(UUID id, boolean isApproved, String alasan);
+    public Pengiriman tolakParsialOlehAdmin(UUID id, String alasan, Double angkutanDiakui);
+
 }
