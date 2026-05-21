452456c72694b5275c8f06797a576dd7a311aa6e
NaviCode <nnavicode@gmail.com>
2026-05-21 14:21:35 +0700

    Edit java version in Dockefile, Add dependencies for prometheus, add metrics in application.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
 	implementation("net.devh:grpc-spring-boot-starter:$grpcSpringStarterVersion")
 	implementation("io.grpc:grpc-protobuf:$grpcVersion")
 	implementation("io.grpc:grpc-stub:$grpcVersion")
